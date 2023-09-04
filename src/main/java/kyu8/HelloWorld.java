3ad2811c4f48dce1bc2c7a933be41d433d4c4f1b
Shan <shantais90@gmail.com>
2023-09-04 09:55:43 +0200

    Added HelloWorld file

---
+++ src/main/java/kyu8/HelloWorld.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+/**
+ *   Write a public static function `greet` that returns "hello world!"
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+    public static String greet(){
+        return "hello world!";
+    }
+
+}
