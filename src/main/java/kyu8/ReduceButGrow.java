1b3c69a6734c321ba980592f1cb84496a2685200
Shan <shantais90@gmail.com>
2023-09-25 22:27:44 +0200

    Added ReduceButGrow class and Test

---
+++ src/main/java/kyu8/ReduceButGrow.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+/**
+ * Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+ *
+ * [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+ */
+
+public class ReduceButGrow {
+    public static int grow(int[] x){
+
+        return 0;
+    }
+}
---
+++ src/test/java/kyu8/ReduceButGrowTest.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReduceButGrowTest {
+    @Test
+    public void testSomething() {
+        assertEquals(6, ReduceButGrow.grow(new int[]{1,2,3}));
+        assertEquals(16, ReduceButGrow.grow(new int[]{4,1,1,1,4}));
+        assertEquals(64, ReduceButGrow.grow(new int[]{2,2,2,2,2,2}));
+    }
+}
