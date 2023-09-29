5547a86cc04e771f84b4268ec02029b25e30f461
Shan <shantais90@gmail.com>
2023-09-29 14:46:25 +0200

    Added RunningOutOfSpace shortened code

--- src/main/java/kyu7/RunningOutOfSpace.java
+++ src/main/java/kyu7/RunningOutOfSpace.java
@@ -2,11 +2,9 @@ package kyu7;
 
 public class RunningOutOfSpace {
     public static String[] spacey(String[] array) {
-        String [] strings = new String[array.length];
-        strings[0] = array[0];
         for (int i=1; i< array.length; i++){
-            strings[i] = strings[i-1]+array[i];
+            array[i] = array[i-1]+array[i];
         }
-        return strings;
+        return array;
     }
 }
