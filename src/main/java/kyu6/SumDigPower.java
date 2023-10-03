e602b4272f12ffeb9f38cc645f8c7e8d57c5113a
Shan <shantais90@gmail.com>
2023-10-03 13:46:57 +0200

    Added SumDigPower excercise with tests.

---
+++ src/main/java/kyu6/SumDigPower.java
@@ -0,0 +1,21 @@
+package kyu6;
+
+import java.util.List;
+
+/**
+ * We need a function to collect these numbers, that may receive two integers aaa, bbb that defines the range [a,b][a, b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+ * Examples
+ * Let's see some cases (input -> output):
+ * 1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * 1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+ * If there are no numbers of this kind in the range
+ * [a,b][a, b][a,b] the function should output an empty list.
+ * 90, 100 --> []
+ * Enjoy it!!
+ */
+
+public class SumDigPower {
+    public static List<Long> sumDigPow(long a, long b) {
+        return null;
+    }
+}
