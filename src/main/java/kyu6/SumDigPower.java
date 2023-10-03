782461a99a0851805f561857c7854fb2e5eb2b84
Shan <shantais90@gmail.com>
2023-10-03 14:46:28 +0200

    Solved SumDigPower.

--- src/main/java/kyu6/SumDigPower.java
+++ src/main/java/kyu6/SumDigPower.java
@@ -1,6 +1,8 @@
 package kyu6;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.LongStream;
 
 /**
  * We need a function to collect these numbers, that may receive two integers aaa, bbb that defines the range [a,b][a, b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
@@ -16,6 +18,14 @@ import java.util.List;
 
 public class SumDigPower {
     public static List<Long> sumDigPow(long a, long b) {
-        return null;
+        return LongStream.range(a,b).filter(digit -> {
+            int [] currentNumber = Arrays.stream(String.valueOf(digit).split("(?!^)"))
+                    .mapToInt(Integer::parseInt).toArray();
+            int sumOfPowers =0;
+            for (int i=0; i<currentNumber.length; i++){
+                sumOfPowers += (int) Math.pow(currentNumber[i],(i+1));
+            }
+            return sumOfPowers == digit;
+        }).boxed().toList();
     }
 }
