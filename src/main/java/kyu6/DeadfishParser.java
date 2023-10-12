bf6651dc2351c740aa2d8af4315bcff68a6959d1
Shan <shantais90@gmail.com>
2023-10-12 11:55:12 +0200

    Solved DeadfishParser. Passed tests.

--- src/main/java/kyu6/DeadfishParser.java
+++ src/main/java/kyu6/DeadfishParser.java
@@ -1,4 +1,41 @@
 package kyu6;
 
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Write a simple parser that will parse and run Deadfish.
+ * Deadfish has 4 commands, each 1 character long:
+ *     i increments the value (initially 0)
+ *     d decrements the value
+ *     s squares the value
+ *     o outputs the value into the return array
+ * Invalid characters should be ignored.
+ */
+
 public class DeadfishParser {
+    public static int[] parse(String data) {
+        List <Integer> results = new LinkedList<>();
+        char [] instructionArray = data.toCharArray();
+        int value = 0;
+        for (char instruction : instructionArray){
+            switch (instruction){
+                case 'i':
+                    value += 1;
+                    break;
+                case 'd':
+                    value -= 1;
+                    break;
+                case 's':
+                    value *= value;
+                    break;
+                case 'o':
+                    results.add(value);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return results.stream().mapToInt(i -> i).toArray();
+    }
 }
--- src/test/java/kyu6/DeadfishParserTest.java
+++ src/test/java/kyu6/DeadfishParserTest.java
@@ -1,7 +1,13 @@
 package kyu6;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeadfishParserTest {
-
+    @Test
+    public void exampleTests() {
+        assertArrayEquals(new int[] {8, 64}, DeadfishParser.parse("iiisdoso"));
+        assertArrayEquals(new int[] {8, 64, 3600}, DeadfishParser.parse("iiisdosodddddiso"));
+    }
 }
