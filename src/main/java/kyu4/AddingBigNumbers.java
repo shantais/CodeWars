c38c30cd921d1e6c0ec48599f65ef2b58a493c2f
Shan <shantais90@gmail.com>
2023-11-05 21:10:58 +0100

    Solved AddingBigNumbers and passed Tests.

--- src/main/java/kyu4/AddingBigNumbers.java
+++ src/main/java/kyu4/AddingBigNumbers.java
@@ -1,6 +1,7 @@
 package kyu4;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 /**
  * We need to sum big numbers, and we require your help.
@@ -16,7 +17,43 @@ import java.math.BigInteger;
  */
 
 public class AddingBigNumbers {
-    public static String add(String a, String b) {
+    public static String add(String a, String b){
+        StringBuilder solution = new StringBuilder();
+        StringBuilder numA = new StringBuilder(a);
+        StringBuilder numB = new StringBuilder(b);
+        int difference = Math.abs(a.length()-b.length());
+
+        if (a.length()>b.length()){
+            numB.insert(0, "0".repeat(difference));
+        } else if (a.length()<b.length()){
+            numA.insert(0, "0".repeat(difference));
+        }
+
+        String[] aArr = numA.reverse().toString().split("");
+        String[] bArr = numB.reverse().toString().split("");
+        int sum;
+        String carry = "0";
+
+        for (int i=0; i<aArr.length; i++){
+            sum = Integer.parseInt(aArr[i])+Integer.parseInt(bArr[i])+Integer.parseInt(carry);
+            System.out.println(sum);
+            String[] tempSum = String.valueOf(sum).split("");
+
+            if (tempSum.length==1){
+                carry = "0";
+                solution.append(tempSum[0]);
+            } else {
+                carry = tempSum[0];
+                solution.append(tempSum[1]);
+            }
+        }
+        if (!carry.equals("0")) {
+            solution.append(Integer.parseInt(carry));
+        }
+        return solution.reverse().toString();
+    }
+
+    public static String addUsingBigInteger(String a, String b) {
         return (new BigInteger(a).add(new BigInteger(b))).toString();
     }
 }
