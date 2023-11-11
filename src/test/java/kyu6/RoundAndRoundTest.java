e94ca568a6996d49fd6b626a03c4a89e4b247a4e
Shan <shantais90@gmail.com>
2023-11-11 22:44:34 +0100

    Solved RoundAndRound solution (worked for codewars, will think why doesn't work on intellij)

--- src/test/java/kyu6/RoundAndRoundTest.java
+++ src/test/java/kyu6/RoundAndRoundTest.java
@@ -4,33 +4,31 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static kyu6.RoundAndRound.roundTo2DecimalPlaces;
-
 class RoundAndRoundTest {
     @Test
     public void exampleTest() {
-        Assertions.assertEquals(2D, roundTo2DecimalPlaces(2D), -1e12D);
-        Assertions.assertEquals(7.48D, roundTo2DecimalPlaces(7.477D), -1e12D);
-        Assert.assertEquals(-5D, roundTo2DecimalPlaces(-4.999D), -1e12D);
-        Assert.assertEquals(18.12D, roundTo2DecimalPlaces(18.123D), -1e12D);
-        Assert.assertEquals(0D, roundTo2DecimalPlaces(0D), -1e12D);
-        Assert.assertEquals(1.46D, roundTo2DecimalPlaces(1.455D), -1e12D);
-        Assert.assertEquals(-1.46D, roundTo2DecimalPlaces(-1.455D), -1e12D);
-        Assert.assertEquals(1.06D, roundTo2DecimalPlaces(1.055D), -1e12D);
-        Assert.assertEquals(-1.06D, roundTo2DecimalPlaces(-1.055D), -1e12D);
-        Assert.assertEquals(16.77D, roundTo2DecimalPlaces(16.765D), -1e12D);
-        Assert.assertEquals(-16.77D, roundTo2DecimalPlaces(-16.765D), -1e12D);
-        Assert.assertEquals(1.03D, roundTo2DecimalPlaces(1.025D), -1e12D);
-        Assert.assertEquals(-1.03D, roundTo2DecimalPlaces(-1.025D), -1e12D);
-        Assert.assertEquals(16.36D, roundTo2DecimalPlaces(16.355D), -1e12D);
-        Assert.assertEquals(-16.35D, roundTo2DecimalPlaces(-16.345D), -1e12D);
-        Assert.assertEquals(-1.19D, roundTo2DecimalPlaces(-1.1949999999999999D), -1e12D);
-        Assert.assertEquals(-1.20D, roundTo2DecimalPlaces(-1.1950000000000001D), -1e12D);
-        Assert.assertEquals(1.19D, roundTo2DecimalPlaces(1.1949999999999999D), -1e12D);
-        Assert.assertEquals(1.20D, roundTo2DecimalPlaces(1.1950000000000001D), -1e12D);
-        Assert.assertEquals(-1.18D, roundTo2DecimalPlaces(-1.1849999999999999D), -1e12D);
-        Assert.assertEquals(-1.19D, roundTo2DecimalPlaces(-1.1850000000000001D), -1e12D);
-        Assert.assertEquals(1.18D, roundTo2DecimalPlaces(1.1849999999999999D), -1e12D);
-        Assert.assertEquals(1.19D, roundTo2DecimalPlaces(1.1850000000000001D), -1e12D);
+        Assertions.assertEquals(2D, RoundAndRound.roundTo2DecimalPlaces(2D), -1e12D);
+        Assertions.assertEquals(7.48D, RoundAndRound.roundTo2DecimalPlaces(7.477D), -1e12D);
+        Assert.assertEquals(-5D, RoundAndRound.roundTo2DecimalPlaces(-4.999D), -1e12D);
+        Assert.assertEquals(18.12D, RoundAndRound.roundTo2DecimalPlaces(18.123D), -1e12D);
+        Assert.assertEquals(0D, RoundAndRound.roundTo2DecimalPlaces(0D), -1e12D);
+        Assert.assertEquals(1.46D, RoundAndRound.roundTo2DecimalPlaces(1.455D), -1e12D);
+        Assert.assertEquals(-1.46D, RoundAndRound.roundTo2DecimalPlaces(-1.455D), -1e12D);
+        Assert.assertEquals(1.06D, RoundAndRound.roundTo2DecimalPlaces(1.055D), -1e12D);
+        Assert.assertEquals(-1.06D, RoundAndRound.roundTo2DecimalPlaces(-1.055D), -1e12D);
+        Assert.assertEquals(16.77D, RoundAndRound.roundTo2DecimalPlaces(16.765D), -1e12D);
+        Assert.assertEquals(-16.77D, RoundAndRound.roundTo2DecimalPlaces(-16.765D), -1e12D);
+        Assert.assertEquals(1.03D, RoundAndRound.roundTo2DecimalPlaces(1.025D), -1e12D);
+        Assert.assertEquals(-1.03D, RoundAndRound.roundTo2DecimalPlaces(-1.025D), -1e12D);
+        Assert.assertEquals(16.36D, RoundAndRound.roundTo2DecimalPlaces(16.355D), -1e12D);
+        Assert.assertEquals(-16.35D, RoundAndRound.roundTo2DecimalPlaces(-16.345D), -1e12D);
+        Assert.assertEquals(-1.19D, RoundAndRound.roundTo2DecimalPlaces(-1.1949999999999999D), -1e12D);
+        Assert.assertEquals(-1.20D, RoundAndRound.roundTo2DecimalPlaces(-1.1950000000000001D), -1e12D);
+        Assert.assertEquals(1.19D, RoundAndRound.roundTo2DecimalPlaces(1.1949999999999999D), -1e12D);
+        Assert.assertEquals(1.20D, RoundAndRound.roundTo2DecimalPlaces(1.1950000000000001D), -1e12D);
+        Assert.assertEquals(-1.18D, RoundAndRound.roundTo2DecimalPlaces(-1.1849999999999999D), -1e12D);
+        Assert.assertEquals(-1.19D, RoundAndRound.roundTo2DecimalPlaces(-1.1850000000000001D), -1e12D);
+        Assert.assertEquals(1.18D, RoundAndRound.roundTo2DecimalPlaces(1.1849999999999999D), -1e12D);
+        Assert.assertEquals(1.19D, RoundAndRound.roundTo2DecimalPlaces(1.1850000000000001D), -1e12D);
     }
 }
