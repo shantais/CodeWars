32ce40c62ab3e7d14744c7569359aa74bf80928e
Shan <shantais90@gmail.com>
2023-11-13 20:40:08 +0100

    Solved EvenOrOdd.

--- src/main/java/kyu8/EvenOrOdd.java
+++ src/main/java/kyu8/EvenOrOdd.java
@@ -1,12 +1,13 @@
 package kyu8;
 
 /**
- * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ * Create a function that takes an integer as an argument
+ * and returns "Even" for even numbers or "Odd" for odd numbers.
  */
 
 public class EvenOrOdd {
 
     public static String evenOrOdd(int i) {
-        return "";
+        return (i % 2 == 0) ? "Even" :  "Odd";
     }
 }
